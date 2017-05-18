4c4077c7b4db9034c3511ec74b8bd0e8b70a2447
radek024 <radek002@o2.pl>
2017-05-19 00:04:11 +0200

    Revert "lets start"
    This reverts commit f5332426af61f418e80f6a5f92130eae719e122e.

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var menu=document.querySelector(".hamburger-menu");
-var navbar=document.querySelector(".page-navigation");
-menu.addEventListener("click", function(){
-var lista=document.querySelector(".nav-list");
-lista.classList.toggle("open");
-menu.classList.toggle("close-menu");
-navbar.classList.toggle("open");
-
-}, false);
