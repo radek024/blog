7b79e6c1934a29327c14a50209a0f4fd4547c5e5
radek024 <radek002@o2.pl>
2017-10-06 01:10:28 +0200

    blog improvments - no-js mode, code styled up

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
-var menu=document.querySelector(".hamburger-menu");
-var navbar=document.querySelector(".page-navigation");
-menu.addEventListener("click", function(){
-var lista=document.querySelector(".nav-list");
-lista.classList.toggle("open");
-menu.classList.toggle("close-menu");
-navbar.classList.toggle("open");
+let menu = document.querySelector(".hamburger-menu");
+let navbar = document.querySelector(".page-navigation");
+
+menu.addEventListener("click", function() {
+    let lista = document.querySelector(".nav-list");
+    lista.classList.toggle("open");
+    menu.classList.toggle("close-menu");
+    navbar.classList.toggle("open");
 
 }, false);
 
-window.addEventListener("load", function(){
-  var tabContent=document.querySelector(".table-content");
-  var subtitles=document.querySelectorAll("h3");
-  if(subtitles.length>1){
-    tabContent.querySelector("p").remove();
-  }
-  var table=document.createElement("ol");
-  table.classList.add("content-list");
-  for(var i=1; i<=subtitles.length-2; i++){
-  var listItem=document.createElement("li");
-  var link=document.createElement("a");
-  link.href="#"+subtitles[i].id;
-  link.innerHTML=subtitles[i].innerHTML;
-  listItem.appendChild(link);
-  table.appendChild(listItem);
-  tabContent.appendChild(table);
-  }
-}, false);
+if (document.querySelector("body").classList.contains("post")) {
+    window.addEventListener("DOMContentLoaded", function() {
+        let tabContent = document.querySelector(".table-content");
+        let subtitles = document.querySelectorAll("h3");
+        if (subtitles.length > 1) {
+            tabContent.querySelector("p").remove();
+        }
+        let table = document.createElement("ol");
+        table.classList.add("content-list");
+        for (let i = 1; i <= subtitles.length - 2; i++) {
+            let listItem = document.createElement("li");
+            let link = document.createElement("a");
+            link.href = "#" + subtitles[i].id;
+            link.innerHTML = subtitles[i].innerHTML;
+            listItem.appendChild(link);
+            table.appendChild(listItem);
+            tabContent.appendChild(table);
+        }
+    }, false);
+}
