4d01dc7687a9448910286a8a82ac79c294436f25
radek024 <rgryczan83@gmail.com>
2023-04-14 00:07:00 +0200

    feat: addded lazyloading to blog

---
+++ _plugins/gen_lazyload.rb
@@ -0,0 +1,13 @@
+module Jekyll
+    class LazyLoadImages < Generator
+      safe true
+      priority :low
+  
+      def generate(site)
+        site.posts.docs.each do |post|
+          post_output = post.content.gsub(/(?<!`)!\[(.*?)\]\((?!.*loading=)(.*?)\)(?!{.*`)/m, "![\\1](\\2){:loading='lazy'}")
+          post.content = post_output
+        end
+      end
+    end
+  end
